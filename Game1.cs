24ce69d575cfcb8b22ad386f6f97c79c979182c5
Naqi Master <701018804@GoTVDSB.ca>
2024-04-29 14:44:01 -0400

    Added Intro Screen (Working on End Screen)

--- Game1.cs
+++ Game1.cs
@@ -10,10 +10,22 @@ namespace Monogame___Animations
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        enum Screen
+        {
+            Intro,
+            TribbleYard,
+            EndScreen
+        }
+        Screen screen;
+        MouseState mouseState;
+        SpriteFont intro;
+
         SoundEffect tribbleSound;
         Random generator = new Random();
         Rectangle window;
-        Texture2D greyTribbleTexture, creamTribbleTexture, orangeTribbleTexture,brownTribbleTexture, windowTexture, seaBackground, spaceBackground;
+        Texture2D greyTribbleTexture, creamTribbleTexture, orangeTribbleTexture,brownTribbleTexture, windowTexture,
+            seaBackground, spaceBackground, tribbleIntroTexture;
         Rectangle greyTribbleRect, creamTribbleRect, orangeTribbleRect, brownTribbleRect;
         Vector2 greyTribbleSpeed, creamTribbleSpeed, orangeTribbleSpeed, brownTribbleSpeed;
 
@@ -27,6 +39,8 @@ namespace Monogame___Animations
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            screen = Screen.Intro;
+
             window = new Rectangle(0,0,800,600);
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
@@ -59,72 +73,83 @@ namespace Monogame___Animations
             seaBackground = Content.Load<Texture2D>("tribbleUnderwater");
             spaceBackground = Content.Load<Texture2D>("tribbleGalaxy");
             tribbleSound = Content.Load<SoundEffect>("tribble_coo");
-
+            tribbleIntroTexture = Content.Load<Texture2D>("tribbleIntro");
+            intro = Content.Load<SpriteFont>("intro");
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
+
+            if (screen == Screen.Intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    screen = Screen.TribbleYard;
+
+            }
+            else if (screen == Screen.TribbleYard)
+            {
+                greyTribbleRect.X += (int)greyTribbleSpeed.X;
+                if (greyTribbleRect.Right > window.Width || greyTribbleRect.Left < 0)
+                {
+                    greyTribbleSpeed.X *= -1;
+                    windowTexture = seaBackground;
+                    tribbleSound.Play();
+                }
+
+                greyTribbleRect.Y += (int)greyTribbleSpeed.Y;
+                if (greyTribbleRect.Top < 0 || greyTribbleRect.Bottom > window.Height)
+                {
+                    greyTribbleSpeed.Y *= -1;
+                    windowTexture = spaceBackground;
+                    tribbleSound.Play();
+                }
+
+
+                creamTribbleRect.X += (int)creamTribbleSpeed.X;
+                creamTribbleRect.Y += (int)creamTribbleSpeed.Y;
+
+                if (creamTribbleRect.Right > window.Width || creamTribbleRect.Left < 0)
+                {
+                    creamTribbleSpeed.X *= -1;
+                    windowTexture = seaBackground;
+                    tribbleSound.Play();
+                }
+
+
+                orangeTribbleRect.X += (int)orangeTribbleSpeed.X;
+                orangeTribbleRect.Y += (int)orangeTribbleSpeed.Y;
+
+
+                if (orangeTribbleRect.Top < 0 || orangeTribbleRect.Bottom > window.Height)
+                {
+                    orangeTribbleSpeed.Y *= -1;
+                    windowTexture = spaceBackground;
+                    tribbleSound.Play();
+                }
+
+
+                brownTribbleRect.X += (int)brownTribbleSpeed.X;
+                brownTribbleRect.Y += (int)brownTribbleSpeed.Y;
+
+                if (brownTribbleRect.Right > window.Width || brownTribbleRect.Left < 0)
+                {
+                    brownTribbleSpeed.X *= -1;
+                    windowTexture = seaBackground;
+                    tribbleSound.Play();
+                }
+                if (brownTribbleRect.Top < 0 || brownTribbleRect.Bottom > window.Height)
+                {
+                    brownTribbleSpeed.Y *= -1;
+                    windowTexture = spaceBackground;
+                    tribbleSound.Play();
+                }
+            }
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
-
-
-            greyTribbleRect.X += (int)greyTribbleSpeed.X;
-            if (greyTribbleRect.Right > window.Width || greyTribbleRect.Left < 0)
-            {
-                greyTribbleSpeed.X *= -1;
-                windowTexture = seaBackground;
-                tribbleSound.Play();
-            }
-
-            greyTribbleRect.Y += (int)greyTribbleSpeed.Y;
-            if (greyTribbleRect.Top < 0 || greyTribbleRect.Bottom > window.Height)
-            {
-                greyTribbleSpeed.Y *= -1;
-                windowTexture = spaceBackground;
-                tribbleSound.Play();
-            }
-
-
-            creamTribbleRect.X += (int)creamTribbleSpeed.X;
-            creamTribbleRect.Y += (int)creamTribbleSpeed.Y;
-
-            if (creamTribbleRect.Right > window.Width || creamTribbleRect.Left < 0)
-            {
-                creamTribbleSpeed.X *= -1;
-                windowTexture = seaBackground;
-                tribbleSound.Play();
-            }
-
-
-            orangeTribbleRect.X += (int)orangeTribbleSpeed.X;
-            orangeTribbleRect.Y += (int)orangeTribbleSpeed.Y;
-
-
-            if (orangeTribbleRect.Top < 0 || orangeTribbleRect.Bottom > window.Height)
-            {
-                orangeTribbleSpeed.Y *= -1;
-                windowTexture = spaceBackground;
-                tribbleSound.Play();
-            }
-
-
-            brownTribbleRect.X += (int)brownTribbleSpeed.X;
-            brownTribbleRect.Y += (int)brownTribbleSpeed.Y;
-
-            if (brownTribbleRect.Right > window.Width || brownTribbleRect.Left < 0)
-            {
-                brownTribbleSpeed.X *= -1;
-                windowTexture = seaBackground;
-                tribbleSound.Play();
-            }
-            if (brownTribbleRect.Top < 0 || brownTribbleRect.Bottom > window.Height)
-            {
-                brownTribbleSpeed.Y *= -1;
-                windowTexture = spaceBackground;
-                tribbleSound.Play();
-            }
 
             base.Update(gameTime);
         }
@@ -136,13 +161,23 @@ namespace Monogame___Animations
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(windowTexture, window, Color.White);
-            _spriteBatch.Draw(greyTribbleTexture,greyTribbleRect,Color.White);
-            _spriteBatch.Draw(creamTribbleTexture,creamTribbleRect,Color.White);
-            _spriteBatch.Draw(orangeTribbleTexture,orangeTribbleRect,Color.White);  
-            _spriteBatch.Draw(brownTribbleTexture,brownTribbleRect,Color.White);
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(tribbleIntroTexture, new Rectangle(0, 0, 800, 600), Color.White);
+                _spriteBatch.DrawString(intro, "Do you want to see the TRIBBLES?", new Vector2(450, 10), Color.Black);
+                _spriteBatch.DrawString(intro, "LEFT CLICK to see them!", new Vector2(470, 30), Color.Black);
 
-            _spriteBatch.End();
+            }
+            else if (screen == Screen.TribbleYard)
+            {
+                _spriteBatch.Draw(windowTexture, window, Color.White);
+                _spriteBatch.Draw(greyTribbleTexture, greyTribbleRect, Color.White);
+                _spriteBatch.Draw(creamTribbleTexture, creamTribbleRect, Color.White);
+                _spriteBatch.Draw(orangeTribbleTexture, orangeTribbleRect, Color.White);
+                _spriteBatch.Draw(brownTribbleTexture, brownTribbleRect, Color.White);
+            }
+
+                _spriteBatch.End();
 
             base.Draw(gameTime);
         }
